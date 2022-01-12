3e1a8230cc930d90d7759250b7a1a36a710bde05
rafinhabusatta <lacerdabusatta@gmail.com>
2022-01-12 12:11:15 -0300

    Criação da página A Fabico;

---
+++ templates/fabico.php
@@ -0,0 +1,10 @@
+<!-- Template name: fabico -->
+<?php get_header(); ?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12">
+      Página A Fabico
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
