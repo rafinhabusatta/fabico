05be5a9e91bdbb616e187e4a651a9ec2e0c45163
rafinhabusatta <lacerdabusatta@gmail.com>
2022-01-22 16:43:18 -0300

    Pesquisa - criação da página; Implementação de parte do conteúdo; Inclusão de doc;

---
+++ templates/pesquisa.php
@@ -0,0 +1,52 @@
+<!-- Template name: Pesquisa -->
+<?php get_header(); ?>
+<div class="container-fluid">
+  <div class="row bg-header">
+    <div class="col-12 bloco-container">
+      <div class="row mx-md-3">
+        <div class="col-12 px-md-0">
+          <h1 class="text-center mb-4">Pesquisa</h1>
+        </div>
+      </div>
+      <div class="row mx-md-3">
+        <div class="col-12 px-md-0 m-xl-auto">
+          <p class="text-justify">
+            As pesquisas na FABICO são desenvolvidas pelos professores dentro do Programa de Pós-Graduação em Comunicação e Informação ou ligadas aos Departamentos aos quais estão vinculados, inscritas no Sistema de Pesquisa da UFRGS como Linhas Individuais.
+          </p>
+          <p class="text-justify">
+            Todos os projetos de pesquisa devem ser encaminhados para a Compesq - Comissão de Pesquisa da Unidade, que atua conjuntamente com a Pró-Reitoria de Pesquisa da Universidade.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row mx-0 mt-3">
+    <div class="col-lg-3 d-none d-lg-block">
+      <nav class="side-menu">
+        <ul>
+          <li><a href="#compesq">Compesq</a></li>
+          <li><a href="#ppgcom">Linhas do PPGCOM</a></li>
+          <li><a href="#normas">Normas e Orientações</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="col-12 col-lg-9">
+      <div class="compesq">
+        <div class="row">
+          <div class="col-12">
+            <div class="row">
+              <div class="col-12">
+                <p class="text-justify">
+                  Comissão de Pesquisa da Faculdade de Biblioteconomia e Comunicação é um órgão da Unidade, constituída por docentes, um representante técnico-administrativo e um representante discente, devidamente escolhidos por seus pares, que fazem a ligação da pesquisa desenvolvida pela Unidade com a Pró-Reitoria de Pesquisa da UFRGS.
+                </p>
+                <a href="<?php bloginfo("template_directory"); ?>/docs/Regimento-Interno--03-06-2016.doc" target="_blank">Regimento Interno da Compesq</a>
+                <a href="<?php bloginfo("template_directory"); ?>/docs/Planejamento-2016-2017.docx" target="_blank">Planejamento da Compesq período 2016-2017</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
