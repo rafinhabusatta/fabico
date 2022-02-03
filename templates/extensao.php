5643cb2804b936bfd6f02f53b583c342c7e5a4da
rafinhabusatta <lacerdabusatta@gmail.com>
2022-02-03 18:48:26 -0300

    Extensão - Implementação de id na div de normas, para utilização correta do menu lateral;

--- templates/extensao.php
+++ templates/extensao.php
@@ -81,7 +81,7 @@
           </div>
         </div>
       </div>
-      <div class="row">
+      <div id="normas" class="row">
         <div class="col-12">
           <div class="row">
             <div class="col-12">
