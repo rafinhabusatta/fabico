116788185ee3b75d6a6dca0b1f4718a154c4a0bd
rafinhabusatta <lacerdabusatta@gmail.com>
2022-08-05 00:54:39 -0300

    Widgets - Inicializacao do primeiro widget;

--- functions.php
+++ functions.php
@@ -170,3 +170,44 @@ function body_contato($classes){
 	}
 	return $classes;
 }
+
+/* ********** WIDGET AREA ********** */
+
+function fabico_widget_register_widget() {
+	register_widget( 'fabico_widget' );
+}
+add_action( 'widgets_init', 'fabico_widget_register_widget' );
+
+class fabico_widget extends WP_Widget {
+	function __construct() {
+		parent::__construct(
+			'fabico_widget', // Base ID
+			__('Fabico Widget', 'text_domain'), // Name
+			array( 'description' => __( 'Primeiro widget teste', 'text_domain' ), ) // Args
+		);
+	}
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		echo __( 'Boas vindas ao nosso primeiro Widget, Fabico!', 'text_domain' );
+		echo $args['after_widget'];
+	}
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php
+	}
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
+
+
+}
